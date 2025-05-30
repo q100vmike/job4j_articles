f95cb7999e50ef0c1533759e121e5dac9ed9e3c8
q100vmike <melkin@mail.ru>
2025-05-30 16:42:42 +0300

    4. Шаблонизатор. [#855]

--- src/main/java/ru/job4j/articles/store/Store.java
+++ src/main/java/ru/job4j/articles/store/Store.java
@@ -3,6 +3,8 @@ package ru.job4j.articles.store;
 import java.util.List;
 
 public interface Store<T> {
+
     T save(T model);
+
     List<T> findAll();
 }
--- src/main/java/ru/job4j/template/TextGenerator.java
+++ src/main/java/ru/job4j/template/TextGenerator.java
@@ -3,6 +3,7 @@ package ru.job4j.template;
 import java.util.Map;
 
 public class TextGenerator implements Generator {
+
     @Override
     public String produce(String template, Map<String, String> args) {
         return "";
