89c8251b47b0e7ee42eb1e35814e1a4b39474977
q100vmike <melkin@mail.ru>
2025-05-30 16:37:13 +0300

    4. Шаблонизатор. [#855]

--- src/main/java/ru/job4j/template/TextGenerator.java
+++ src/main/java/ru/job4j/template/TextGenerator.java
@@ -2,7 +2,7 @@ package ru.job4j.template;
 
 import java.util.Map;
 
-public class TextGenerator implements Generator{
+public class TextGenerator implements Generator {
     @Override
     public String produce(String template, Map<String, String> args) {
         return "";
