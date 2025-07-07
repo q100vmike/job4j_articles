3e2a9172ba42b77121e7d961f3ddcc4b9d5c931b
q100vmike <melkin@mail.ru>
2025-07-07 14:29:16 +0300

    1. Поддержка форматов XML, JSON в генераторе отчетов. [#851]

--- src/test/java/ru/job4j/ood/srp/report/XMLReportEngineTest.java
+++ src/test/java/ru/job4j/ood/srp/report/XMLReportEngineTest.java
@@ -26,23 +26,21 @@ class XMLReportEngineTest {
         store.add(employee);
         store.add(employee1);
         Report report = new XMLReportEngine(store);
-        String expect = """
-                <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-                <employees>
-                    <employee>
-                        <name>John Doe</name>
-                        <hired>08:06:2023 17:41</hired>
-                        <fired>08:06:2023 17:41</fired>
-                        <salary>5000.0</salary>
-                    </employee>
-                    <employee>
-                        <name>Jane Smith</name>
-                        <hired>08:06:2023 17:41</hired>
-                        <fired>08:06:2023 17:41</fired>
-                        <salary>6000.0</salary>
-                    </employee>
-                </employees>
-                """;
+        String expect = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                + "<employees>\n"
+                + "    <employee>\n"
+                + "        <name>John Doe</name>\n"
+                + "        <hired>08:06:2023 17:41</hired>\n"
+                + "        <fired>08:06:2023 17:41</fired>\n"
+                + "        <salary>5000.0</salary>\n"
+                + "    </employee>\n"
+                + "    <employee>\n"
+                + "        <name>Jane Smith</name>\n"
+                + "        <hired>08:06:2023 17:41</hired>\n"
+                + "        <fired>08:06:2023 17:41</fired>\n"
+                + "        <salary>6000.0</salary>\n"
+                + "    </employee>\n"
+                +"</employees>\n";
 
         assertThat(report.generate(em -> true)).isEqualTo(expect);
     }
