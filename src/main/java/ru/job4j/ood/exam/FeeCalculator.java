258acb3779f15ab5d7100305eeda5623e9616694
q100vmike <melkin@mail.ru>
2025-12-19 15:03:48 +0300

    тестовая задача ООД

--- src/main/java/ru/job4j/ood/exam/FeeCalculator.java
+++ src/main/java/ru/job4j/ood/exam/FeeCalculator.java
@@ -1,16 +1,19 @@
 package ru.job4j.ood.exam;
 
 public class FeeCalculator {
+
     private boolean clientType = false;
     private double fee = 0;
 
     public FeeCalculator(boolean clientType) {
         this.clientType = clientType;
-        this.fee = 0.015;
+        this.fee = clientType ? 0 : 0.015;
     }
 
     public void setFee(double fee) {
-        this.fee = fee;
+        if (!this.clientType) {
+            this.fee = fee;
+        }
     }
 
     public double getFee() {
